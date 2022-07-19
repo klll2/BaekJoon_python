94594af9656a4607106dd18d668deb0d95d81b55
klll2 <60285296+klll2@users.noreply.github.com>
2022-07-19 19:45:51 +0900

    Add files via upload
    7/19 시간초과 떠서 정답참고함.
    해설 적음.

---
+++ G1_1016_제곱ㄴㄴ수(정답참고).py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+li = [1]*(b-a+1)
+for i in range(2, int(b**0.5)+1):
+    t = i**2
+    for j in range(a//t*t, b+1, t): 
+        # a//t*t -> 보정(ex.a = 15 t = 16이면 a = 16부터 시작)
+        # t -> 배수(상동일 때, ㅓ = 16, 20, 24, ....)
+        if j >= a and li[j-a]:
+            # ㅓ >= a -> 범위 충족
+            # li[j-a] -> 리스트는 b-a+1개이므로,
+            li[j-a] = 0
+            # boolean 배열로 중복을 없앰.
+print(li.count(1))
